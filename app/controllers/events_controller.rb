5eb0684e444c210c71a3c7ed5145d328307ade61
Brian Tai <alphabt@gmail.com>
2015-12-24 00:15:47 +0800

    Added XML, JSON, and ATOM formats to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,13 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.page(params[:page]).per(5)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @events.to_xml }
+      format.json { render :json => @events.to_json }
+      format.atom { @feed_title = "My event list" } # index.atom.builder
+    end
   end
 
   def new
@@ -20,7 +27,11 @@ class EventsController < ApplicationController
   end
 
   def show
-    @page_title = @event.name
+    respond_to do |format|
+      format.html { @page_title = @event.name }
+      format.xml # show.xml.builder
+      format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+    end
   end
 
   def edit
